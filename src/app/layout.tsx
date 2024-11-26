c3741732b6b6d85b91613a536a68759ca953be10
Ulises Estrada <ulisesestradaceja@gmail.com>
2024-11-25 19:17:58 -0600

    Tarjetas e iconos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <MainNav items={dashboardConfig.mainNav}/>
+        <MainNav items={dashboardConfig.mainNav} />
         <Providers>{children}</Providers>
       </body>
     </html>
