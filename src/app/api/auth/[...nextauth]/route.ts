da624b4339fdcf38b99bf2fbc3cce4baa3684c09
ErickMendez01 <erickmendezgdl@gmail.com>
2025-05-19 19:20:55 -0600

    build error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,23 @@
-export { GET, POST } from  "@/auth"
+import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+  ...authConfig,
+});
+
+export const GET = handler;
+export const POST = handler;
