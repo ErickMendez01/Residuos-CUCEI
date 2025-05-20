3c91c83c3757c9e77a22ca8e777dbc4334193a59
ErickMendez01 <erickmendezgdl@gmail.com>
2025-05-19 19:39:37 -0600

    fix error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,19 +4,22 @@ import authConfig from "@/auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
-const authHandler = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "jwt",
-  },
+export const {
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
   callbacks: {
     async session({ session, token }) {
-      if (token.sub && session.user) {
+      if(token.sub && session.user) {
         session.user.id = token.sub;
       }
       return session;
     },
   },
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
   ...authConfig,
 });
-
