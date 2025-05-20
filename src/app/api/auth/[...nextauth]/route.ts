4e6b23d6547ad3610a0786520f7494e4a291ebb2
ErickMendez01 <erickmendezgdl@gmail.com>
2025-05-19 19:46:12 -0600

    authconfig

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,7 @@
-export { GET, POST } from  "@/auth"
-import NextAuth from "next-auth";
-import authConfig from "@/auth.config";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/prisma";
+import NextAuth from "next-auth"
 
-export const {
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
-  callbacks: {
-    async session({ session, token }) {
-      if(token.sub && session.user) {
-        session.user.id = token.sub;
-      }
-      return session;
-    },
-  },
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "jwt",
-  },
-  ...authConfig,
-});
+import authConfig from "@/auth.config";
+
+const handler = NextAuth(authConfig)
+
+export { handler as GET, handler as POST }
